d3aa54308769cb6e45ab0da835d63fd77c88c160
Jose Maradiaga <josemarad25@gmail.com>
2020-01-27 17:21:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,6 +39,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  update_element_from_index [1] = 'Update'
+  update_element_from_index [2] = 'Update'
   =>'Update'
 end
